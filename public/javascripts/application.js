f26dda5b3c872a86df9d89aa585498ae0225eaa0
Stefan Goodchild <me@stefangoodchild.com>
2012-02-10 19:53:16 +0000

    Adding fitvids and bbc youtube video

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	$(".main_header").fitText(1.33);
 	$(".main_subtitle").fitText(2);
 	$("article.main h1").fitText(1.5);
+	$("article.main").fitVids();
 	
 	$('.carousel').carousel({
 		slider: '.slider',
