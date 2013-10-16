a4f4ebc76084517271cae1f68d719970667bddb3
Stefan Goodchild <me@stefangoodchild.com>
2013-10-16 14:46:27 +0100

    Add in showreel to site

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,16 +2,5 @@ $(document).ready(function() {
 	$(".main_header").fitText(1.33);
 	$(".main_subtitle").fitText(2);
 	$("article.main h1").fitText(1.5);
-	$("article.main").fitVids();
 	
-	$('.carousel').carousel({
-		slider: '.slider',
-		slide: '.slide',
-		addPagination: false,
-		addNav: true,
-		nextSlide: '.next',
-		prevSlide: '.prev',
-		speed: 300 // ms.
-	});
-
 });
